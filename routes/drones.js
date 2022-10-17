f36f29365cdf7572548df6794fd0bff2a3bec447
José Ignacio <joseignacioig9910@gmail.com>
2022-10-17 18:00:20 +0200

    lab

--- routes/drones.js
+++ routes/drones.js
@@ -36,7 +36,6 @@ router.get('/drones/:id/edit', (req, res, next) => {
       editDrone
     })
     
-
   })
   .catch((err)=>{
     next(err)
@@ -44,13 +43,30 @@ router.get('/drones/:id/edit', (req, res, next) => {
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params
+  const {name, propellers, maxSpeed} = req.body
+
+  const droneUpdate = {
+    name,
+    propellers,
+    maxSpeed
+  }
+
+  Drone
+  .findByIdAndUpdate(id, droneUpdate)
+  .then(() => {
+    res.redirect("/drones")
+  })
+  .catch((err) => next(err))
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  Drone
+  .findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect("/drones")
+  })
+  .catch((err) => next(err))
 });
 
 module.exports = router;
